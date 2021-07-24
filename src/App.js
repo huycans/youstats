acf862a2451f1067a13444fe74418e2efdcb91c0
huycans <huycan_lk@yahoo.com.vn>
2021-07-23 21:43:32 -0400

    Setup basic redux

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import React, { useEffect, useState } from 'react';
-
-import logo from './logo.svg';
+import { useSelector, useDispatch } from 'react-redux';
 import './App.css';
 import { GoogleLogin } from 'react-google-login';
 import { verifyToken } from "./components/auth";
+import { signInSuccess, resetAuth } from './components/slices/auth';
 
 function App() {
 
   //states of the app
+  // loading state
   let [signedin, setSignedin] = useState(false);
-  let [googleToken, setGoogleToken] = useState("");
+  // let [googleToken, setGoogleToken] = useState("");
+  let googleToken = useSelector(state => state.auth.tokenId);
+  const dispatch = useDispatch();
 
   /**
    * Reset all the states in the app to default values and remove any token in localstorage
@@ -17,30 +20,30 @@ function App() {
    */
   const resetState = () => {
     setSignedin(false);
-    setGoogleToken("");
+    dispatch(resetAuth);
     localStorage.removeItem("google_tokenID");
   };
 
-  useEffect(() => {
-    const signinCheck = async () => {
-      let google_tokenId = localStorage.getItem("google_tokenID");
-      if (google_tokenId) {
-        let isTokenValid = await verifyToken(google_tokenId);
-        if (isTokenValid) {
-          setSignedin(true);
-          setGoogleToken(google_tokenId);
-        }
-        else {
-          resetState();
-        }
-      }
-      else {
-        resetState();
-      }
+  // useEffect(() => {
+  //   const signinCheck = async () => {
+  //     let google_tokenId = localStorage.getItem("google_tokenID");
+  //     if (google_tokenId) {
+  //       let isTokenValid = await verifyToken(google_tokenId);
+  //       if (isTokenValid) {
+  //         setSignedin(true);
+  //         // dispatch(signInSuccess())
+  //       }
+  //       else {
+  //         resetState();
+  //       }
+  //     }
+  //     else {
+  //       resetState();
+  //     }
 
-    };
-    signinCheck();
-  }, []);
+  //   };
+  //   signinCheck();
+  // }, []);
 
 
   const handleSignin = (userProfile) => {
@@ -48,8 +51,9 @@ function App() {
 
     //save the tokenId for client
     localStorage.setItem("google_tokenID", userProfile.tokenId);
+    const { tokenId, profileObj, tokenObj } = userProfile;
     setSignedin(true);
-    setGoogleToken(userProfile.tokenId);
+    dispatch(signInSuccess({ tokenId, profileObj, tokenObj }));
   };
 
   return (
@@ -66,6 +70,7 @@ function App() {
             onSuccess={handleSignin}
             onFailure={handleSignin}
             cookiePolicy={'single_host_origin'}
+            isSignedIn={true}
           />
 
         }
---
+++ src/components/slices/auth.js
@@ -0,0 +1,33 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {
+  tokenId: "",
+  accessToken: "",
+  email: "",
+  name: "",
+  googleId: ""
+};
+
+export const authSlice = createSlice({
+  name: "auth",
+  initialState: initialState,
+  reducers: {
+    signInSuccess: (state, action) => {
+      const { tokenId, profileObj, tokenObj } = action.payload;
+      const { email, name, googleId } = profileObj;
+      const { accessToken } = tokenObj;
+      state.tokenId = tokenId;
+      state.email = email;
+      state.name = name;
+      state.googleId = googleId;
+      state.accessToken = accessToken;
+
+    },
+    resetAuth: (state) => {
+      state = initialState;
+    }
+  }
+});
+
+export const { signInSuccess, resetAuth } = authSlice.actions;
+export default authSlice.reducer;
