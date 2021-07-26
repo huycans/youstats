85cea69fbb7fadea5524840784cd7c989ed72deb
huycans <huycan_lk@yahoo.com.vn>
2021-07-26 11:28:45 -0400

    Add channel search API and reducers

---
+++ src/components/slices/youtube.js
@@ -0,0 +1,28 @@
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import { youtubeAPI } from '../API/youtube';
+const initialState = {
+  channelResults: []
+};
+
+//action as thunk
+export const fetchYoutubeChannelsByKeyword = createAsyncThunk("youtube/fetchChannelsByKeyword",
+  async (keyword, thunkAPI) => {
+    return await youtubeAPI.fetchYoutubeChannelsByKeyword(keyword);
+  }
+);
+
+export const youtubeSlice = createSlice({
+  name: "youtube",
+  initialState: initialState,
+  reducers: {
+
+  },
+  extraReducers: (builder) => {
+    builder.addCase(fetchYoutubeChannelsByKeyword.fulfilled, (state, action) => {
+      state.channelResults = action.payload.items;
+    });
+  }
+});
+
+// export const {  } = youtubeSlice.actions;
+export default youtubeSlice.reducer;
--- src/components/store.js
+++ src/components/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
 import authReducer from './slices/auth';
+import youtubeReducer from './slices/youtube';
 
 export default configureStore({
   reducer: {
-    auth: authReducer
+    auth: authReducer,
+    youtube: youtubeReducer
   }
 });
