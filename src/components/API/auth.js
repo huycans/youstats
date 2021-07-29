a5a85f175832f20f731fc9337402ea33b0ee0e90
huycans <huycan_lk@yahoo.com.vn>
2021-07-28 20:41:09 -0400

    Refactor API constants

--- src/components/API/auth.js
+++ src/components/API/auth.js
@@ -1,6 +1,8 @@
+import { googleURL } from "./constants";
+
 const verifyToken = async (googleToken) => {
   let response = await fetch(
-    "https://oauth2.googleapis.com/tokeninfo?id_token=" + googleToken
+    googleURL.googleOAuth + "?id_token=" + googleToken
   );
   let responseJson = await response.json();
   if (responseJson.error) return false;
--- src/components/API/constants.js
+++ src/components/API/constants.js
@@ -1,4 +1,5 @@
 export const googleURL = {
+  googleOAuth: "https://oauth2.googleapis.com/tokeninfo",
   youtubeSearch: "https://www.googleapis.com/youtube/v3/search",
   youtubeChannelSearch: "https://youtube.googleapis.com/youtube/v3/channels"
 };
